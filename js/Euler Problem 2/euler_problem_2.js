25dcd9a79137aa84e8d53996f74db5bc961897d8
Neil Gehani <njgonline@me.com>
2014-02-23 13:27:12 -0800

    A slightly more elegant solution to Euler 2

--- js/Euler Problem 2/euler_problem_2.js
+++ js/Euler Problem 2/euler_problem_2.js
@@ -1,19 +1,21 @@
-var sequence = [];
-sequence.push(1,2);
-var sum = 0;
+var sum_of_even_numbers = function(max){
+	var sequence = [];
+	sequence.push(0,1,0);
+	var sum = 0;
+	
 
-
-document.write("length of array = "+sequence.length+"<br>");
-for (i=0; i <= 50; i+=1) {
-	sequence.push(sequence[i]+sequence[i+1]);
-	document.write(sequence[i]+"<br>");
-	document.write("length of array ="+sequence.length+"<br>")
-	if (sequence[i]%2 === 0) {
-		sum = sum + sequence[i];
-		if (sum > 4000000) {
-			document.write("sum of even numbers = "+sum+"<br>");
-			return sum;
+	while(sequence[2] <= max){
+		sequence[2] = sequence[0] + sequence[1];
+		if (sequence[2]%2 === 0 && sequence[2] <=max){
+			sum += sequence[2];
+			document.write("running total of even's = "+sum+"<br><br>");	
 		}
-	};	
-};	
+			sequence[0]=sequence[1];
+			sequence[1]=sequence[2];
+			
+	}
+	document.write("sum_of_even_numbers = "+sum);
+	return sum;
+};
 
+sum_of_even_numbers(4000000);
